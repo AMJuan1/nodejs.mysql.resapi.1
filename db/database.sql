ab5d5218ba2eeb80b01f54d84017e0a1743f2818
AMJuan1 <juantony794@gmail.com>
2023-06-30 10:21:01 -0600

    first commit

---
+++ db/database.sql
@@ -0,0 +1,19 @@
+CREAT DATABASE IF NOT EXISTS companydb;
+
+USE companydb;
+
+CREATE TABLE employee (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) DEFAULT NULL,
+    salary INT(5) DEFAULT NULL,
+    PRIMARY KEY (id)
+
+);
+
+DESCRIBE employee;
+
+INSERT INTO employee VALUES 
+(1,'Joe',1000),
+(2,'Kevin',2),
+(3,'mike',443),
+(4,'Ross',222);
